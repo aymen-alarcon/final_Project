af6ea9673bc1bd92ff5b160ec9c1596e72432a8e
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-10-01 17:20:10 +0100

    Added additional input validation and purchase logic to gestion_achat.c

--- gestion_achat.c
+++ gestion_achat.c
@@ -7,8 +7,11 @@ void buyProduct()
 {
     int choice;
     int found = 0;
+    int numberrequested = 1;
     printf("can you please enter the products id that you want to buy: ");
     scanf("%d", &choice);
+    printf("can you please enter the products id that you want to buy: ");
+    scanf("%d", &numberrequested);
 
     for (int i = 0; i < 10; i++)
     {
@@ -22,12 +25,21 @@ void buyProduct()
             else if (products[i].stock <= 0)
             {
                 printf("Transaction failed. Product is out of stock.\n");
+            }            
+            else if (products[i].stock < numberrequested || numberrequested <= 0)
+            {
+                printf("Transaction failed. Only %d items left in stock.\n", products[i].stock);
+                
+            }
+            else if (products[i].prix * numberrequested > client[1].balance)
+            {
+                printf("Transaction failed. You do not have enough balance to buy %d items.\n", numberrequested);
             }
             else
             {
-                products[i].stock -= 1;
-                client[1].balance -= products[i].prix;
-                printf("You have successfully purchased %s for %.2f\n", products[i].nom, products[i].prix);
+                products[i].stock -= numberrequested;
+                client[1].balance -= products[i].prix * numberrequested;
+                printf("You have successfully purchased %d x %s for %.2f\n", numberrequested, products[i].nom, products[i].prix * numberrequested);
                 printf("Your new balance is: %.2f\n", client[1].balance);
             }
         }
